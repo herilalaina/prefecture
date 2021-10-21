e65338c218bc1248b222db0dadcd60e92afd0549
HR <rkt.herilalaina@gmail.com>
2021-10-21 21:33:10 +0200

    refractor

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Prefecture
 
-* Add "geckodriver" to PATH: e.g. ``export PATH=$PATH:/home/herilalaina/Documents/Code/prefecture/``
-* Install splinter: "pip install --user splinter"
-  Install selenium: "pip install -U selenium"
-* Execute: ``python main.py --type NAT`` (For testing ``python main.py --type DEC``). 
+
+1.  Add the source code directory to PATH: e.g. ``export PATH=$PATH:/home/blabla/blabla/blabla/prefecture/``
+2. Install selenium: ``pip install -U selenium``
+3. Run: ``python main.py --url https://www.essonne.gouv.fr/booking/create/23014/ --delay 5
+`` (for testing purpose ``python main.py --url http://www.essonne.gouv.fr/booking/create/30986/ --delay 4``).
+
+```
+> python main.py -h
+usage: main.py [-h] --url URL [--delay DELAY]
+
+RDV Prefecture
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --url URL             Link of the RDV webpage. e.g:
+                        https://www.essonne.gouv.fr/booking/create/23014/
+  --delay DELAY, -d DELAY
+                        Delay in second between request.
+```
+
+
+NB: Increase the delay ``--delay`` if you encounter more "Bad gateway" error.
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import sys, os
+import os
 import time
 import random
 import argparse
 from selenium import webdriver
-from twilio.rest import Client
-from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 
-# req_proxy = RequestProxy()  # you may get different number of proxy when  you run this at each time
-# proxies = req_proxy.get_proxy_list()  # this will create proxy list
+try:
+    from twilio.rest import Client
+except ImportError:
+    print("Whatsapp notification not available. Please install and configure Twilio.")
 
-sys.path.append(os.sep.join([os.environ.get("ROOT_APP_PREFECTURE")]))
 
 
 def send_notification():
@@ -25,51 +24,51 @@ def send_notification():
 
 
 def reload(_browser):
-    page_ok = ("naturalisation" in _browser.find_element_by_tag_name('html').text.lower())
+    # True if not bad gateway
+    page_ok = ("française" in _browser.find_element_by_tag_name('html').text.lower())
     c = 0
     while (not page_ok) and (c < 10):
         _browser.refresh()
-        page_ok = ("naturalisation" in _browser.find_element_by_tag_name('html').text.lower())
+        page_ok = ("française" in _browser.find_element_by_tag_name('html').text.lower())
         c += 1
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='RDV Prefecture')
     parser.add_argument(
-        '--type', '-t',
-        help='Type RDV (NAT, DEC)',
+        '--url',
+        help='Link of the RDV webpage. e.g: https://www.essonne.gouv.fr/booking/create/23014/',
         type=str,
-        choices=["NAT", "DEC"],
         required=True
+    )
+    parser.add_argument(
+        '--delay', '-d',
+        help='Delay in second between request.',
+        type=int,
+        default=30,
     )
     args = parser.parse_args()
 
     browser = webdriver.Firefox()
     found = False
-    if args.type == "NAT":
-        url = 'http://www.essonne.gouv.fr/booking/create/23014/'
-        porte_list = ["planning23018", "planning23198", "planning23199", "planning23200", "planning23201"]
-    elif args.type == "DEC":
-        url = 'http://www.essonne.gouv.fr/booking/create/30986/'
-        porte_list = ["planning30987", "planning30988"]
-    else:
-        raise Exception("Error type RDV.")
 
     while not found:
         try:
-            browser.get(url)
+            browser.get(args.url)
             browser.find_element_by_id("condition").click()
-            element = browser.find_element_by_name("nextButton")
-            browser.execute_script("arguments[0].click();", element)
+            browser.find_element_by_name("nextButton").click()
             reload(browser)
             while not found:
+                porte_list = [elem.get_attribute("id") for elem in browser.find_elements_by_name("planning")]
                 random.shuffle(porte_list)
                 for id in porte_list:
-                    time.sleep(4)
+                    time.sleep(args.delay)
                     browser.find_element_by_id(id).click()
                     reload(browser)
+
                     try:
                         browser.find_element_by_name("nextButton").click()
+                        reload(browser)
                         if browser.find_element_by_id("inner_Booking").find_element_by_tag_name(
                                 "h2").text == 'Description de la nature du rendez-vous':
                             browser.find_element_by_name("nextButton").click()
@@ -83,5 +82,7 @@ if __name__ == "__main__":
             browser.delete_all_cookies()
             time.sleep(60)
 
-    # Notification ici
-    send_notification()
+    try:
+        send_notification()
+    except:
+        print("FOUND RDV.")
